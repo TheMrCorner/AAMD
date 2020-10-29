ed1f0d7bf162d23eb2663ca34ab1e77270774ff0
Alejandro <alpermar98@gmail.com>
2020-10-29 01:06:51 +0100

    Calculo de la integral terminado en practica0, dibujos de los puntos y las funciones, hay que quitarlo o ponerlo de otra manera

--- Practica 0/practica0.py
+++ Practica 0/practica0.py
@@ -1,10 +1,11 @@
+# Cálculo de la integral por el método de Monte Carlo
 import numpy as np
 import matplotlib.pyplot as plt
 import time as t 
 
-
+# FUncion de prueba
 def funcion(x):
-    return x + 1
+    return -(x * x) + 50
 
 # Comprueba que esta debajo de la curva
 def esta_debajo(fun, x, y):
@@ -18,52 +19,66 @@ def gen_puntos(min_n, max_n, num_puntos):
 
     return a
 
-def integraL(fun, a, b, num_puntos=1000):
+def draw_graphic(x, y, ry):
+    plt.figure()
+    plt.subplot(1, 1, 1)
+    plt.plot(x, y, color='blue')
+    plt.scatter(x[np.where(ry > y)], ry[np.where(ry > y)], marker='x', color='red')
+    plt.scatter(x[(ry < y)], ry[(ry < y)], marker='o', color='green')
+    plt.show()
+
+def guillotina(fun, a, b, num_puntos=1000):
     # Puntos x de la curva principal
     x = np.linspace(a, b, num_puntos)
-    y = funcion(x)
-    m = max(y)
+    y = fun(x)
 
     # Puntos random, coordenada X
-    #rx = gen_puntos(a, b, num_puntos)
-    ry = gen_puntos(0, max(y), num_puntos)
+    ry = gen_puntos(min(y), max(y), num_puntos)
 
-    a = 0
+    draw_graphic(x, y, ry)
+
+    return y, ry
+
+def integraL(fun, a, b, y, ry):
+
+    count = 0
     #comienza el cronometro
     inicio = t.process_time()
-    for i in range(num_puntos):
+    for i in range(len(ry)):
         if ry[i] < y[i]:
-            a += 1
+            count += 1
+
+    #print del resultado de la integral
+    print("Resultado integral: " + str(count/len(ry) * (b - a) * max(y)))
+
     #detiene el cronometro
     fin = t.process_time()
-    print(a)
+
+    print("Total de trues (L): " + str(count))
+
     #tiempo que ha tardado
     tiempo = 1000 * (fin- inicio)
-    print(tiempo)
+    print("Tiempo que ha tomado la funcion lenta: " + str(tiempo))
     return tiempo
-    #a = gen_puntos(num_puntos)
 
-def integraR(fun, a, b, num_puntos=1000):
-    # Puntos x de la curva principal
-    x = np.linspace(a, b, num_puntos)
-    y = funcion(x)
-    m = max(y)
+def integraR(fun, a, b, y, ry):
 
-    # Puntos random, coordenada X
-    #rx = gen_puntos(a, b, num_puntos)
-    ry = gen_puntos(0, max(y), num_puntos)
     #comienza el cronometro
     inicio = t.process_time()
     # Método rápido
     res = np.sum(ry < y)
+
+    print("Resultado integral: " + str(res/len(ry) * (b - a) * max(y)))
+
+    print("Total de trues (R): " + str(res))
+
     #detiene el cronometro
     fin = t.process_time()
-    print(res)
+    
     #tiempo que ha tardado
     tiempo = 1000 * (fin- inicio)
-    print(tiempo)
+    print("Tiempo que ha tomado la funcion rápida: " + str(tiempo))
     return tiempo
-    #a = gen_puntos(num_puntos)
 
 #crea una tuple de cantidades de puntos con los que va a probar
 sizes = np.linspace(1000, 1000000, 5)
@@ -71,8 +86,9 @@ tiempo_bucle = []
 tiempo_numpy = []
 #calcula el tiempo de los algoritmos para todas las cantidades de puntos
 for size in sizes:
-    tiempo_bucle += [integraL(funcion, 2, 8, int(size))]
-    tiempo_numpy += [integraR(funcion, 2, 8, int(size))]
+    y, ry = guillotina(funcion, 2, 3000, int(size))
+    tiempo_bucle += [integraL(funcion, 3000, 8, y, ry)]
+    tiempo_numpy += [integraR(funcion, 3000, 8, y, ry)]
 
 #dibuja el resultado
 plt.figure()
@@ -82,7 +98,5 @@ plt.legend()
 plt.show()
 
 #hay que usar sum(y < f(x)) para la versión vectorizada
-
-
 
 # Función que va a hacer la integral
